27c966de65ac7d434dd95a0a1213305e7c60b707
MaiklDudikov <wmiwa87@gmail.com>
2024-10-09 19:59:58 +0200

    create app 'hello' in django

--- draft_lesson_command_django.py
+++ draft_lesson_command_django.py
@@ -1,3 +1,7 @@
 print("lesson academy TOP")
 # 1. pip install django
 # 2. django-admin startproject first_project
+# 3. cd first_project
+# 4. python manage.py runserver
+
+# 5. python manage.py startapp hello
