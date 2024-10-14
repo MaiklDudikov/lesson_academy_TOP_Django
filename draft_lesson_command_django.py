374b8e9057fee7d5f7a0af4c35502de705123dd5
MaiklDudikov <wmiwa87@gmail.com>
2024-10-14 21:17:24 +0200

    added lesson 14.10.2024

--- draft_lesson_command_django.py
+++ draft_lesson_command_django.py
@@ -1,5 +1,7 @@
 print("first commands Django")
 
+# pip list
+
 # 1. pip install django
 # 2. django-admin startproject first_project
 
@@ -7,3 +9,21 @@ print("first commands Django")
 # 4. python manage.py runserver
 
 # 5. python manage.py startapp hello
+# Зарегистрируйте приложение hello в файле first_project/settings.py
+# Добавить файл urls.py приложение hello
+
+
+
+# Тут закончил
+
+
+
+# 6. Создание моделей в models.py и потом миграции :
+# python manage.py makemigrations
+# python manage.py migrate
+
+# 7. python manage.py createsuperuser (mishawin11/12345678)
+# 8. зарегистрировать таблицу в admin.py
+
+# Исправление blog_project/urls.py и blog/urls.py
+
