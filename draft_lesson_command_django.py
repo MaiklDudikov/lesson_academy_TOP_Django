77515eac626ee88f690169cc4a3b274869231cb8
MaiklDudikov <wmiwa87@gmail.com>
2024-10-15 11:57:20 +0200

    added lesson 14.10 update draft_lesson

--- draft_lesson_command_django.py
+++ draft_lesson_command_django.py
@@ -12,11 +12,7 @@ print("first commands Django")
 # Зарегистрируйте приложение hello в файле first_project/settings.py
 # Добавить файл urls.py приложение hello
 
-
-
 # Тут закончил
-
-
 
 # 6. Создание моделей в models.py и потом миграции :
 # python manage.py makemigrations
