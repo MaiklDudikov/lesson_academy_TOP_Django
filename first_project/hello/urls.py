7f86d2075af955630c82be015d7cb4ba1dbcfe54
MaiklDudikov <wmiwa87@gmail.com>
2024-10-23 19:42:21 +0200

    added 05_lesson_23.10.24

--- first_project/hello/urls.py
+++ first_project/hello/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about/", views.about),
-    path("contact/", views.contact),
-    path("details/", views.details),
-    path("index/<int:id>", views.index),
-    path("access/<int:age>", views.access),
+    path('', views.index),
 ]
--- first_project/hello/views.py
+++ first_project/hello/views.py
@@ -1,36 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
+from django.shortcuts import render
 
 
-def about(request):
-    return HttpResponse("About")
-
-
-def contact(request):
-    return HttpResponse("/about")
-
-
-def details(request):
-    return HttpResponse("/")
-
-
-def index(request, id):
-    people = ["Tom", "Bob", "Sam"]
-    # если пользователь найден, возвращаем его
-    if id in range(0, len(people)):
-        return HttpResponse(people[id])
-    # если нет, то возвращаем ошибку 404
-    else:
-        return HttpResponseNotFound("Not Found")
-
-
-def access(request, age):
-    # если возраст НЕ входит в диапазон 1-110, посылаем ошибку 400
-    if age not in range(1, 111):
-        return HttpResponseBadRequest("Некорректные данные")
-    # если возраст больше 17, то доступ разрешен
-    if (age > 17):
-        return HttpResponse("Доступ разрешен")
-    # если нет, то возвращаем ошибку 403
-    else:
-        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+def index(request):
+    return render(request, 'hello/index.html')
