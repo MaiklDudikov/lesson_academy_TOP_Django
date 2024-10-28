26cf8fa215e3a48f33de3d9a41bc6b0b84fd575a
MaiklDudikov <wmiwa87@gmail.com>
2024-10-28 20:52:19 +0200

    added lesson_06.2

--- first_project/hello/views.py
+++ first_project/hello/views.py
@@ -4,16 +4,26 @@ from .forms import UserForm
 
 
 def index(request):
-    return render(request, 'hello/index.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        return HttpResponse(f"<h2>Привет, {name}, твой возраст : {age}</h2>")
+    else:
+        userform = UserForm()
+        return render(request, "hello/index.html", {"form": userform})
 
 
-def postuser(request):
-    # получаем из данных запроса POST отправленные через форму данные
-    name = request.POST.get("name", "Undefined")
-    age = request.POST.get("age", 1)
-    langs = request.POST.getlist("languages", ["python"])
+# def index(request):
+#     return render(request, 'hello/index.html')
 
-    return HttpResponse(f"""
-                <div>Name : {name}<br>  Age : {age}</div><br>
-                <div>Languages : {langs}</div>
-            """)
+
+# def postuser(request):
+#     # получаем из данных запроса POST отправленные через форму данные
+#     name = request.POST.get("name", "Undefined")
+#     age = request.POST.get("age", 1)
+#     langs = request.POST.getlist("languages", ["python"])
+#
+#     return HttpResponse(f"""
+#                 <div>Name : {name}<br>  Age : {age}</div><br>
+#                 <div>Languages : {langs}</div>
+#             """)
