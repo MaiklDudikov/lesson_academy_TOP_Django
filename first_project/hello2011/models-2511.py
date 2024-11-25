92e8e972f38acc30536067c08b0fc55b7c17dac3
MaiklDudikov <wmiwa87@gmail.com>
2024-11-25 20:26:56 +0200

    added 13_lesson_25.11.2024_v2

---
+++ first_project/hello2011/models-2511.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
